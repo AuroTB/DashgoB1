dab3866cf8fb49cf3604a976dd5a59227cf64853
AuroTB <auro.tj@gmail.com>
2021-05-21 03:17:02 -0500

    Added initialized functional repo

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,16 @@
+# CMake generated Testfile for 
+# Source directory: /home/eaibot/dashgo_ws/src
+# Build directory: /home/eaibot/dashgo_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(dashgo/dashgo_bringup)
+subdirs(dashgo/dashgo_description)
+subdirs(dashgo/dashgo_driver)
+subdirs(dashgo/dashgo_nav)
+subdirs(dashgo/dashgo_rviz)
+subdirs(dashgo/dashgo_tools)
+subdirs(laser_filters)
+subdirs(robot_pose_publisher)
+subdirs(dashgo/ydlidar_v1.3.1)
---
+++ build/laser_filters/CMakeFiles/laser_filters_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laser_filters_gencfg"
+  "/home/eaibot/dashgo_ws/devel/include/laser_filters/IntensityFilterConfig.h"
+  "/home/eaibot/dashgo_ws/devel/share/laser_filters/docs/IntensityFilterConfig.dox"
+  "/home/eaibot/dashgo_ws/devel/share/laser_filters/docs/IntensityFilterConfig-usage.dox"
+  "/home/eaibot/dashgo_ws/devel/lib/python2.7/dist-packages/laser_filters/cfg/IntensityFilterConfig.py"
+  "/home/eaibot/dashgo_ws/devel/share/laser_filters/docs/IntensityFilterConfig.wikidoc"
+  "/home/eaibot/dashgo_ws/devel/include/laser_filters/PolygonFilterConfig.h"
+  "/home/eaibot/dashgo_ws/devel/share/laser_filters/docs/PolygonFilterConfig.dox"
+  "/home/eaibot/dashgo_ws/devel/share/laser_filters/docs/PolygonFilterConfig-usage.dox"
+  "/home/eaibot/dashgo_ws/devel/lib/python2.7/dist-packages/laser_filters/cfg/PolygonFilterConfig.py"
+  "/home/eaibot/dashgo_ws/devel/share/laser_filters/docs/PolygonFilterConfig.wikidoc"
+  "/home/eaibot/dashgo_ws/devel/include/laser_filters/ScanShadowsFilterConfig.h"
+  "/home/eaibot/dashgo_ws/devel/share/laser_filters/docs/ScanShadowsFilterConfig.dox"
+  "/home/eaibot/dashgo_ws/devel/share/laser_filters/docs/ScanShadowsFilterConfig-usage.dox"
+  "/home/eaibot/dashgo_ws/devel/lib/python2.7/dist-packages/laser_filters/cfg/ScanShadowsFilterConfig.py"
+  "/home/eaibot/dashgo_ws/devel/share/laser_filters/docs/ScanShadowsFilterConfig.wikidoc"
+  "/home/eaibot/dashgo_ws/devel/include/laser_filters/SpeckleFilterConfig.h"
+  "/home/eaibot/dashgo_ws/devel/share/laser_filters/docs/SpeckleFilterConfig.dox"
+  "/home/eaibot/dashgo_ws/devel/share/laser_filters/docs/SpeckleFilterConfig-usage.dox"
+  "/home/eaibot/dashgo_ws/devel/lib/python2.7/dist-packages/laser_filters/cfg/SpeckleFilterConfig.py"
+  "/home/eaibot/dashgo_ws/devel/share/laser_filters/docs/SpeckleFilterConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/laser_filters_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
